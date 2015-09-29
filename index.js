38268e61368dd8a1cae42578bccd6eba4437e46e
Raif Harik <reharik@gmail.com>
2015-09-28 19:05:56 -0500

    getting ready for the int tests

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
  * Created by reharik on 8/13/15.
  */
 
-var Trainer = require('./src/AggregateRoots/Trainer');
-var AggregateRootBase = require('./src/AggregateRoots/AggregateRootBase');
-
 var extend = require('extend');
 var registry = require('./registry');
 
@@ -16,8 +13,6 @@ module.exports = function(_options) {
     };
     extend(options, _options || {});
     var container = registry(options);
-    var plugin = container.getInstanceOf('eventRepository');
-
-    return plugin(options);
+    return container.getHashOfGroup('aggregateroots');
 };
 
--- registry.js
+++ registry.js
@@ -5,11 +5,11 @@ var dagon = require('dagon');
 
 module.exports = function(_options) {
     var options = _options || {};
-    var container = dagon(options.dagon);
+    var container = dagon(options.dagon||{});
     return new container(x=>
         x.pathToRoot(__dirname)
             .requireDirectoryRecursively('./src')
-            .groupAllInDirectory('./src/AggregateRoots', 'AggregateRoots_hash')
+            .groupAllInDirectory('./src/AggregateRoots', 'aggregateroots')
             .for('bluebird').renameTo('Promise')
             .for('corelogger').renameTo('logger').instanciate(i=>i.asFunc().withParameters(options.logger || {}))
             .complete());
