9ba694f765b0baf628a2e46a2efc000e04abbb13
Raif Harik <reharik@gmail.com>
2016-01-14 21:07:20 -0600

    fixing up reg

--- app/index.js
+++ app/index.js
@@ -12,7 +12,6 @@ module.exports = function(_options) {
         }
     };
     extend(options, _options || {});
-    var container = registry(options);
-    return container.getHashOfGroup('aggregateroots');
+    return  registry(options);
 };
 
--- app/registry.js
+++ app/registry.js
@@ -1,18 +1,17 @@
 /**
  * Created by parallels on 9/3/15.
  */
+"use strict";
+
 var dagon = require('dagon');
 var path = require('path');
 
 module.exports = function(_options) {
     var options = _options || {};
-    var container = dagon(options.dagon);
-    return new container(x=>
+    var registry = dagon(options.dagon).registry;
+    return registry(x=>
         x.pathToRoot(path.join(__dirname,'..'))
             .requireDirectoryRecursively('./app/src')
             .groupAllInDirectory('./app/src/AggregateRoots', 'aggregateroots')
-            .for('eventmodels').instantiate(i=>i.asFunc())
-            .for('bluebird').renameTo('Promise')
-            .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
             .complete());
 };
