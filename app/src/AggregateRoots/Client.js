1ae75415f682e57c11f045d5605339308be337fb
Raif Harik <reharik@gmail.com>
2017-02-15 21:57:32 -0600

    missed adding id to client on addclient

--- app/src/AggregateRoots/Client.js
+++ app/src/AggregateRoots/Client.js
@@ -18,6 +18,7 @@ module.exports = function(AggregateRootBase, invariant, uuid) {
         commandHandlers() {
             return {
                 'addClient'   : function(cmd) {
+                    cmd.id = uuid.v4();
                     this.raiseEvent({
                         eventName     : 'clientAdded',
                         data          : cmd
